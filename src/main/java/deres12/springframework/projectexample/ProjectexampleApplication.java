579fd90efe185a584996c1f9608552ab8fbe8891
Stefano Carrino <stefano.carrino@gruppomcr.com>
2022-07-27 11:30:29 +0200

    init commit

---
+++ src/main/java/deres12/springframework/projectexample/ProjectexampleApplication.java
@@ -0,0 +1,13 @@
+package deres12.springframework.projectexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/deres12/springframework/projectexample/ProjectexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package deres12.springframework.projectexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
