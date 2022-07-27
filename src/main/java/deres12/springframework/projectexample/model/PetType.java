2ce05d7308f76e211ca0d50b768dcd77f7b1a1fd
Stefano Carrino <stefano.carrino@gruppomcr.com>
2022-07-27 11:41:29 +0200

    create model

---
+++ src/main/java/deres12/springframework/projectexample/model/PetType.java
@@ -0,0 +1,14 @@
+package deres12.springframework.projectexample.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
