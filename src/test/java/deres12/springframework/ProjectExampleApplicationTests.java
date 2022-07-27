db822ac37d12ecd8524bfd75f7de724216d2ac3d
Stefano Carrino <stefano.carrino@gruppomcr.com>
2022-07-27 12:11:13 +0200

    rename test

--- src/test/java/deres12/springframework/ProjectExampleApplicationTests.java
+++ src/test/java/deres12/springframework/ProjectExampleApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectexampleApplicationTests {
+class ProjectExampleApplicationTests {
 
 	@Test
 	void contextLoads() {
